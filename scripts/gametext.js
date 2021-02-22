5581ce92d986ed724d69b3f7301b1f5352a9ff32
Oliver Coldwell <olivercoldwell@gmail.com>
2021-02-22 22:43:04 +0000

    functionality comolete, brain melter

--- scripts/gametext.js
+++ scripts/gametext.js
@@ -1,30 +1,52 @@
 export const firstLocationText = {
-  bedroomStartText: ['Hi! My name is Ruby, I\'ve just woken up and no-one is here! What do I do?', "To make a choice in this game you use the keyboard and pick from two choices, 1 and 2", 'I can see a ball and a slipper, which do I choose?', 'Ball = 1, Slipper = 2'],
-  bedroomChoiceOne: ["I've picked up the ball and now I need someone to throw it, where's Dad?", "I need to eat and I don't think a ball is edible, do I try?", "Eat ball or not"],
-  bedroomChoiceTwo: ["I've got this slipper, it smells nice", "If people see me with this item I will be in trouble.", "Do I chew the slipper or move on to that other smell"],
-}
+  bedroomStartText: [
+    "Hi! My name is Ruby, I've just woken up and no-one is here! What do I do?",
+    "To make a choice in this game you use the keyboard and pick from two choices, 1 and 2",
+    "I can see a ball and a slipper, which do I choose?",
+    "Ball = 1, Slipper = 2",
+  ],
+  bedroomChoiceOne: [
+    "I've picked up the ball and now I need someone to throw it, where's Dad?",
+    "I need to eat and I don't think a ball is edible, do I try?",
+    "Eat ball(1) or run to dad(2)",
+  ],
+  bedroomChoiceTwo: [
+    "I've got this slipper, it smells nice",
+    "If people see me with this item I will be in trouble.",
+    "Do I chew the slipper(1) or move on to that other smell(2)",
+  ],
+};
+//Choice One
 export const firstChoiceLocationText = {
-  landingChoiceOne: [],
-  landingChoiceTwo: []
-}
-export const landingChoiceOneText = {
-  endingOne: [],
-  endingTwo: []
-}
-export const landingChoiceTwoText = {
-  endingThree: [],
-  endingFour: []
-}
+  landingChoiceOne: [
+    "I've eaten the ball, this was a mistake.",
+    "Be Sick(1) or Get Help from Dad(2)",
+  ],
+  endingOne: [
+    "I've been sick and luckily the ball came out",
+    "On hearing you being sick, your Mum walks in and sees your sick all over the floor, 'You're a bad dog, Ruby' ",
+    "You are ashamed of yourself. GAME OVER 111",
+  ],
+  endingTwo: [
+    "You run downstairs and try to bark, all that comes out is a slight squeak, your Dad looks at you funny",
+    "You try to communicate with him again but you fail. He takes you to the vets and you get the ball removed",
+    "You aren't really sure how it happened, you fell asleep and you woke up without the squeak and apparently you had a diamond in your stomach. YOU ARE A GOOD DOG 112",
+  ],
+  landingChoiceTwo: [
+    "I've eaten the ball, and now I squeak when I breath, what fun.",
+    "Find Dad(1) or Go exploring(2)",
+  ],
+  endingThree: [
+    "You find your dad in the bathroom and squeak at him in a series of attempted barks 121",
+  ],
+  endingFour: ["I am a placeholder for now 122"],
+};
+//Choice Two
 export const secondChoiceLocationText = {
-  bathroomChoiceOne: [],
-  bathroomChoiceTwo: []
-}
-
-export const bathroomChoiceOneText = {
-  endingFive: [],
-  endingSix: []
-}
-export const bathroomChoiceTwoText = {
-  endingSeven: [],
-  endingEight: []
-}
+  bathroomChoiceOne: ["I am a placeholder for 21", "Choose one or two"],
+  endingFive: ["I am a placeholder for 211"],
+  endingSix: ["I am a placeholder for  212"],
+  bathroomChoiceTwo: ["I am a placeholder for 22", "choose one or two"],
+  endingSeven: ["I am a placeholder for now 221"],
+  endingEight: ["I am a placeholder for now I am plaholder for 222"],
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,92 +1,147 @@
-import {firstLocationText, firstChoiceLocationText, landingChoiceOneText,  landingChoiceTwoText, secondChoiceLocationText, bathroomChoiceOneText, bathroomChoiceTwoText} from './gametext.js'
+import {
+  firstLocationText,
+  firstChoiceLocationText,
+  secondChoiceLocationText,
+} from "./gametext.js";
 let time = 0;
-let gameText = {firstChoiceLocationText, landingChoiceOneText, landingChoiceTwoText, secondChoiceLocationText, bathroomChoiceOneText, bathroomChoiceTwoText}
+let choiceTrack = null;
 const keyHandler = (event) => {
-  if(event.key == 'Enter' && time == 0) {
-   console.log("Good dog, you're able to follow commands");
-   time ++;
-   startGame();
-  } else if (event.key == '1' && currentRoom.length == storyIndex + 1) {
+  console.log(time);
+  //Stops type animation if already playinh
+  if (isTyping == true) {
+    return;
+    //Initalises game
+  } else if (event.key == "Enter" && time == 0) {
+    console.log("Good dog, you're able to follow commands");
+    time++;
+    startGame();
+  } else if (event.key == "1" && time >= 2) {
+    choiceIndex = 0;
+    resetNewArray();
+    choiceTrack = choiceTrack + "1";
+    if (choiceTrack === "11") {
+      arrayChoiceSelector(firstChoiceLocationText, 0);
+    } else if (choiceTrack === "21") {
+      arrayChoiceSelector(secondChoiceLocationText, 0);
+    } else if (choiceTrack === "111") {
+      arrayChoiceSelector(currentObject, 1);
+    } else if (choiceTrack === "121") {
+      arrayChoiceSelector(currentObject, 4);
+    } else if (choiceTrack === "211") {
+      arrayChoiceSelector(currentObject, 1);
+    } else if (choiceTrack === "221") {
+      arrayChoiceSelector(currentObject, 4);
+    }
+    console.log(choiceTrack);
+    time++;
+  } else if (event.key == "2" && time >= 2) {
+    choiceIndex = 0;
+    resetNewArray();
+    choiceTrack = choiceTrack + "2";
+    if (choiceTrack === "12") {
+      arrayChoiceSelector(firstChoiceLocationText, 3);
+    } else if (choiceTrack === "22") {
+      arrayChoiceSelector(secondChoiceLocationText, 3);
+    } else if (choiceTrack === "112") {
+      arrayChoiceSelector(currentObject, 2);
+    } else if (choiceTrack === "122") {
+      arrayChoiceSelector(currentObject, 5);
+    } else if (choiceTrack === "212") {
+      arrayChoiceSelector(currentObject, 2);
+    } else if (choiceTrack === "222") {
+      arrayChoiceSelector(currentObject, 5);
+    }
+    console.log(choiceTrack);
+    time++;
+    //sets choiceTrack to 1 or 2
+  } else if (event.key == "1" && currentRoom.length == storyIndex + 1) {
     choiceIndex = 1;
     resetNewArray();
-    arrayChoiceSelector(currentObject, choiceIndex)
-  }else if (event.key == '2' && currentRoom.length == storyIndex + 1) {
-     choiceIndex =2;
-     resetNewArray();
-     arrayChoiceSelector(currentObject, choiceIndex)
-  }else if (event.key == 'Enter' && currentRoom.length == storyIndex+1) {
+    arrayChoiceSelector(currentObject, choiceIndex);
+    time++;
+    choiceTrack = "1";
+  } else if (event.key == "2" && currentRoom.length == storyIndex + 1) {
+    choiceIndex = 2;
+    resetNewArray();
+    arrayChoiceSelector(currentObject, choiceIndex);
+    time++;
+    choiceTrack = "2";
+    //Story progression through array on enter
+  } else if (event.key == "Enter" && currentRoom.length == storyIndex + 1) {
     storyIndex = 0;
-  } else if (event.key =='Enter' && time > 0) {
+  } else if (event.key == "Enter" && time > 0) {
     updateDisplay();
+    isTyping = true;
     typeWriter(currentRoom[storyIndex]);
-  
-}
-}
+  }
+};
 
 const startGame = () => {
   document.getElementById("onkeypress__active").classList.add("hidden");
   document.getElementById("#ruby-dog").classList.add("hidden");
   document.getElementById("console__field").classList.remove("hidden");
+  document.getElementById("console__field").classList.add("active");
   arrayChoiceSelector(firstLocationText, choiceIndex);
-}
+};
 
 //Typewriter function elements. Setting index outside of scope.
 const speed = 10;
 let indexOfText = 0;
-const typeWriter= text => {
+let isTyping = false;
+const typeWriter = (text) => {
   if (indexOfText < text.length) {
     setTimeout(() => {
       typeWriter(text);
-      document.getElementById("story__text").innerHTML += text.charAt(indexOfText);
+      document.getElementById("story__text").innerHTML += text.charAt(
+        indexOfText
+      );
       indexOfText++;
     }, speed);
   } else if (indexOfText == text.length) {
+    isTyping = false;
     return;
   }
- 
-}
+};
 const updateDisplay = () => {
   storyIndex++;
   document.getElementById("story__text").innerHTML = "";
-  indexOfText= 0;
-}
+  indexOfText = 0;
+};
 const resetNewArray = () => {
-  storyIndex = 0
+  storyIndex = 0;
   document.getElementById("story__text").innerHTML = "";
-  indexOfText= 0;
-}
-let currentObject = {}
-let currentRoom = []
-let choiceIndex = 0
-let storyIndex = 0
+  indexOfText = 0;
+};
+let currentObject = {};
+let currentRoom = [];
+let choiceIndex = 0;
+let storyIndex = 0;
 //Recursive selector that works through story objects and arrays.
 const arrayChoiceSelector = (object, index) => {
-  currentObject = object
+  currentObject = object;
   const arrayOfLocationProperties = Object.keys(object);
-  let choice = arrayOfLocationProperties[index]
-  let currArr = object[choice]
+  let choice = arrayOfLocationProperties[index];
+  let currArr = object[choice];
 
   if (choiceIndex < currArr.length) {
+    isTyping = true;
     typeWriter(currArr[storyIndex]);
     currentRoom = currArr;
-  } 
-  else if (storyIndex == currArr.length) {
-   storyIndex ++
+  } else if (storyIndex == currArr.length) {
+    storyIndex++;
   }
-}
+};
 
+window.addEventListener("keydown", keyHandler);
+//needs user input --> needs to be submitted.
+//provide commands at end of sentence.
+//provide output depending on commands entered (hidden commands?)
 
-
-window.addEventListener("keydown", keyHandler)
- //needs user input --> needs to be submitted.
- //provide commands at end of sentence.
- //provide output depending on commands entered (hidden commands?)
-
- //How many branching choices? Start with two in each room?
-//             /\
-//            /\/\
-//           /\/\/\
-// This should provide 6 end scenarios based on path taken.
+//How many branching choices? Start with two in each room?
+//             /  \
+//            /\  /\
+//           /\/\/\/\
+// This should provide 8 end scenarios based on path taken.
 //This is basic structure of the game.
 //Each choice needs to be unique. Provide two choices each time and then expand.
 //Provide other choices that don't do anything in particular?
@@ -96,5 +151,3 @@ window.addEventListener("keydown", keyHandler)
 // next steps
 //text animation -> on initialisation + inputs.
 // branching paths--> boolean state switches
-
-
