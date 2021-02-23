eaa530b0eea9b0f88470072975894f8647776297
Oliver Coldwell <olivercoldwell@gmail.com>
2021-02-23 16:13:28 +0000

    added final storylines, enjoy the ridiculousness

--- scripts/gametext.js
+++ scripts/gametext.js
@@ -17,7 +17,7 @@ export const firstLocationText = {
 };
 //Choice One
 export const firstChoiceLocationText = {
-  landingChoiceOne: [
+  ballChoiceOne: [
     "I've eaten the ball, this was a mistake.",
     "Be Sick(1) or Get Help from Dad(2)",
   ],
@@ -31,7 +31,7 @@ export const firstChoiceLocationText = {
     "You try to communicate with him again but you fail. He takes you to the vets and you get the ball removed",
     "You aren't really sure how it happened, you fell asleep and you woke up without the squeak and apparently you had a diamond in your stomach. YOU ARE A GOOD DOG 112",
   ],
-  landingChoiceTwo: [
+  ballChoiceTwo: [
     "I run outside of my den and can't see my Dad, but I can smell him but there's also a smell that I recognise and I start to drool",
     "Find Dad(1) or Go exploring(2)",
   ],
@@ -42,10 +42,10 @@ export const firstChoiceLocationText = {
 };
 //Choice Two
 export const secondChoiceLocationText = {
-  bathroomChoiceOne: ["I chew the slipper and I am able to ingest the smell of my parents so that they will always be with me.", "They don't seem to like that logic much", "You can decide to move on to the living room (1) or follow the smell (2) "],
+  slipperChoiceOne: ["I chew the slipper and I am able to ingest the smell of my parents so that they will always be with me.", "They don't seem to like that logic much", "You can decide to move on to the living room (1) or follow the smell (2) "],
   endingFive: ["You arrive at the living room to find a new someone waiting for you, it's glassy eyes stare at you and you decide this is the perfect thing to bite", "'Who's a good girl', you hear, after biting the creature for a few minutes and you get some scritches from your mum. YOU WIN!!"],
   endingSix: ["You follow the smell into the kitchen and see 'CAKE'", "You acquire the CAKE", "You consume the CAKE and gain 10HP and EXP", "You subsume into your true form as DOGGO. YOU WIN!!" ],
-  bathroomChoiceTwo: ["I am a placeholder for 22", "choose one or two"],
-  endingSeven: ["I am a placeholder for now 221"],
-  endingEight: ["I am a placeholder for now I am plaholder for 222"],
+  slipperChoiceTwo: ["I decide to risk it and take the slipper forward as a talisman to ward off evil.", "You may ask what is evil in a dogs world but I saw this pointy eared thing with a long sinuous tail and I decided to bark at it,that is evil", "Find the cat (1) or follow the smell(2)"],
+  endingSeven: ["You fail to find the cat but you do find a well with someone down it. You bark and bark and people come to find you.", "They start to call you Lassie, you are confused and you seem to be a long way from home and even your universe.", "It's your call if this is a win or a loss. Maybe it's the start of a new adventure game franchise."],
+  endingEight: ["You follow the smell to the kitchen and see the CAKE", "You leave your slipper where it is and are able to reach the side where the CAKE is located", "You attempt to eat the CAKE but you didn't realise it was GLUTEN FREE", "THE CAKE IS A LIE, GAME OVER!!"],
 };
